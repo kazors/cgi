8c5fe14edb07f5332de908716e092bdf013df128
kazors <jerome.gerard02@outlook.fr>
2017-05-01 12:10:29 +0200

    oui

---
+++ oracleBuilderParser/src/com/cgi/objectModel/oraclePushButton.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cgi.objectModel;
+
+/**
+ *
+ * @author moi
+ */
+public class oraclePushButton {
+    String m_oraclePushButtonName;
+    String m_oraclePushButtonItemType;
+    String m_oraclePushButtonComments;
+    String m_oraclePushButtonEnabled;
+    String m_oraclePushButtonLabel;
+    String m_oraclePushButtonAccessKey;
+    String m_oraclePushButtonIconic;
+    String m_oraclePushButtonIconFilename;
+    String m_oraclePushButtonDefaultButton;
+    String m_oraclePushButtonPopupMenu;
+    String m_oraclePushButtonKeyboardNavigable;
+    String m_oraclePushButtonMouseNavigate;
+    String m_oraclePushButtonPreviousNavigationItem;
+    String m_oraclePushButtonNextNavigationItem;
+    String m_oraclePushButtonNumberOfItemDisplayed;
+    String m_oraclePushButtonVisible;
+    String m_oraclePushButtonXPosition;
+    String m_oraclePushButtonYPosition;
+    String m_oraclePushButtonWidth;
+    String m_oraclePushButtonHeight;
+    String m_oraclePushButtonForegroundColor;
+}
